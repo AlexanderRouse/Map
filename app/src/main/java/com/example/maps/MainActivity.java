1e3d3220fe64d3fb11e36ab20056d3b8d1bed24d
Alexander Rouse <acrouse@usc.edu>
2022-03-17 15:32:29 -0700

    Uploading map

---
+++ app/src/main/java/com/example/maps/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.maps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap)
+    {
+        map = googleMap;
+        LatLng usc = new LatLng(34.02528414431285
+                , -118.28585527462162);
+        map.addMarker(new MarkerOptions().position(usc).title("USC Village"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(usc));
+    }
+}
